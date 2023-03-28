9f3455d3285e517f927a933a0a2852137fc17ee7
rloldl <you@example.comii319@naver.comgit config --global user.email you@example.comii319@naver.com>
2023-03-28 23:28:41 +0900

    230328_최은비

--- 최은비/main/models.py
+++ 최은비/main/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=20)
-    content = models.TextField()
-    
+    postname = models.CharField(max_length=20)
+    mainphoto = models.ImageField(blank=True, null=True)
+    contents = models.TextField()
+
     def __str__(self):
-        return self.title
+        return self.postname
--- 최은비/main/views.py
+++ 최은비/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Post
 
 # Create your views here.
@@ -18,6 +19,34 @@ def posting(request, num):
     post = Post.objects.get(pk=num)
     context = {
         'post': post,
-        'pk':num,
     }
     return render(request, 'main/posting.html', context)
+
+
+def new_post(request):
+    if request.method == "POST":
+        if request.POST['mainphoto']:
+            new_article=Post.objects.create(
+                postname=request.POST['postname'],
+                contents=request.POST['contents'],
+                mainphoto=request.POST['mainphoto'],
+            )
+        else:
+            new_article=Post.objects.create(
+                postname=request.POST['postname'],
+                contents=request.POST['contents'],
+                mainphoto=request.POST['mainphoto'],
+            )
+        return redirect('/blog/')
+    return render(request, 'main/new_post.html')
+
+
+def remove_post(request, num):
+    post = Post.objects.get(pk=num)
+    context = {
+        'post': post,
+    }
+    if request.method == 'POST':
+        post.delete()
+        return redirect('/blog/')
+    return render(request, 'main/remove_post.html', context)
