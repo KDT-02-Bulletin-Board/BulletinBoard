7715e80969692d0b86fe9e9b495fde153dc5e3fc
최수현 <sw92522@naver.com>
2023-04-23 16:01:02 +0900

    230423_최수현 Create urls, views, templates of accounts

--- 최수현/accounts/views.py
+++ 최수현/accounts/views.py
@@ -1,3 +1,107 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import CustomAuthenticationForm, CustomUserCreationForm, CustomUserChangeForm, CustomPasswordChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import get_user_model
+from diaries.models import Diary, Comment
+from django.db.models import Count, Prefetch
 
-# Create your views here.
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('diaries:index')
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('diaries:index')
+    else:
+        form = CustomAuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('diaries:index')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('diaries:index')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+@login_required
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('diaries:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+@login_required
+def withdraw(request):
+    request.user.delete()
+    auth_logout(request)
+    return redirect('diaries:index')
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = CustomPasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('diaries:index')
+    else:
+        form = CustomPasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
+
+
+def profile(request, username):
+    person = get_user_model().objects.select_related('followings').prefetch_related(
+        'followers', 'diary_set', 'like_diaries',
+        Prefetch('comment_set', queryset=Comment.objects.select_related('diary'))
+        ).annotate(Count('followings'), Count('followers')).get(username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+def follow(request, you_pk):
+    me = request.user
+    you = get_user_model().objects.get(pk=you_pk)
+    if me != you:
+        if me in you.followers:
+            you.followers.remove(me)
+        else:
+            you.followers.add(me)
+    return redirect('accounts:profile', you)
